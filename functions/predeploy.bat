d7af51ab22a530fd599464d9a7f5fa338700fda5
Viswajith K S <viswajithks@ug.cusat.ac.in>
2025-06-11 10:16:45 +0530

    added python env

---
+++ functions/predeploy.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Setting up Python virtual environment...
+call .\venv\Scripts\activate.bat
+pip install -r requirements.txt
---
+++ functions/start_server.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting Flask server...
+cd /D %~dp0
+python main.py
